5bfdcf5519ae66490d20b4d4107cf08681da58ad
hboff <oskar@baucampus.de>
2023-05-05 12:10:59 +0200

    Oskar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -170,7 +170,59 @@ w3-padding w3-display-middle w3-xxlarge" style="background: rgba(255, 255, 255, 
 <body id="myPage">
 
 
-
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0" style=" backdrop-filter: blur(25px) invert(.2);  background-color: rgba(255, 255, 255, .15);">
+        <div class="container">
+            <a class="navbar-brand" href="#!"><span style="color: #5e9693;">Pantheon</span><span
+                    style="color: #fff;">Elite</span></a>
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+                data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#!">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#!">Über Uns</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#!">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#!">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#!">Kontakt</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav d-flex flex-row">
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://api.whatsapp.com/send/?phone=971561642909&text&type=phone_number&app_absent=0">
+                            <i class="fab fa-whatsapp"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://t.me/Baucampus_bot">
+                            <i class="fab fa-telegram"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://api.whatsapp.com/send/?phone=971561642909&text&type=phone_number&app_absent=0">
+                            EN 
+                        </a>
+                    </li><li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://api.whatsapp.com/send/?phone=971561642909&text&type=phone_number&app_absent=0">
+                            RU
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<!-- Navbar -->
     <div class="w3-top w3-text-white">
 
         <div class="w3-bar w3-text-white" id="myNavbar">
