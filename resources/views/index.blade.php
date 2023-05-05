7c421a68ef646a534a8ff71382d12da5337edd82
hboff <oskar@baucampus.de>
2023-05-05 12:43:14 +0200

    test nav

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -165,65 +165,96 @@ w3-padding w3-display-middle w3-xxlarge" style="background: rgba(255, 255, 255, 
   from {opacity: .4}
   to {opacity: 1}
 }
+
+
+/* Color of the links BEFORE scroll */
+.navbar-scroll .nav-link,
+.navbar-scroll .navbar-toggler-icon,
+.navbar-scroll .navbar-brand {
+  color: #fff;
+}
+
+/* Color of the links AFTER scroll */
+.navbar-scrolled .nav-link,
+.navbar-scrolled .navbar-toggler-icon,
+.navbar-scrolled .navbar-brand {
+  color: #fff;
+}
+
+/* Color of the navbar AFTER scroll */
+.navbar-scroll,
+.navbar-scrolled {
+  background-color: #cbbcb1;
+}
+
+.mask-custom {
+  backdrop-filter: blur(5px);
+  background-color: rgba(255, 255, 255, .15);
+}
+
+.navbar-brand {
+  font-size: 1.75rem;
+  letter-spacing: 3px;
+}
 </style>
 </head>
 <body id="myPage">
-
-
-
-    <div class="w3-top w3-text-white">
-
-        <div class="w3-bar w3-text-white" id="myNavbar">
-            <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-left"
-                href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
-                Baucampus
+ <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+    <div class="container">
+      <a class="navbar-brand" href="#!"><span style="color: #5e9693;">Psycho</span><span style="color: #fff;">logist</span></a>
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Offer</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Reference</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Contact</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav d-flex flex-row">
+          <li class="nav-item me-3 me-lg-0">
+            <a class="nav-link" href="#!">
+              <i class="fas fa-shopping-cart"></i>
             </a>
-
-            <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right"
-                href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
-                Nav<i class="fa fa-bars"></i>
+          </li>
+          <li class="nav-item me-3 me-lg-0">
+            <a class="nav-link" href="#!">
+              <i class="fab fa-twitter"></i>
             </a>
-
-            <a href="https://baucampus.com" class="w3-hide-small w3-left hover-underline-animation oskkk"
-                style="padding-left:100px;">Baucampus</a>
-            <a href="https://baucampus.com/kontakt" class=" w3-hide-small w3-right hover-underline-animation oskkk"
-                style="padding-right:100px;">Kontakt</a>
-        </div>
-
-        <!-- Navbar on small screens -->
-        <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-            <a href="https://baucampus.com" class="w3-bar-item w3-button" onclick="toggleFunction()">Baucampus</a>
-            <a href="https://baucampus.com/kontakt" class="w3-bar-item w3-button" onclick="toggleFunction()">Kontakt</a>
-        </div>
+          </li>
+          <li class="nav-item me-3 me-lg-0">
+            <a class="nav-link" href="#!">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <script>
-        // Change style of navbar on scroll
-        window.onscroll = function () {
-            myFunction()
-        };
+  </nav>
+  <!-- Navbar -->
 
-        function myFunction() {
-            var navbar = document.getElementById("myNavbar");
-            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-                navbar.className = "w3-bar" + " w3-animate-top" + " w3-osk-customs";
-            } else {
-                navbar.className = navbar.className.replace(" w3-animate-top w3-osk-customs", "");
-            }
-        }
 
-        // Used to toggle the menu on small screens when clicking on the menu button
-        function toggleFunction() {
-            var x = document.getElementById("navDemo");
-            if (x.className.indexOf("w3-show") == -1) {
-                x.className += " w3-show";
-            } else {
-                x.className = x.className.replace(" w3-show", "");
-            }
-        }
-
-    </script>
-
-    <body>
         <div class="w3-display-container w3-animate-opacity w3-center" id="1">
 
             <img src="pictures/8.jpg" alt="Mein Haus"
